7907d7b99481f383dba007075fb9dbff1f7e8f61
THEDEVILBOSS10 <145843784+THEDEVILBOSS10@users.noreply.github.com>
2025-05-10 22:06:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const fileInput = document.getElementById('fileInput');
 const preview = document.getElementById('preview');
+const imageLink = document.getElementById('imageLink');
 
 fileInput.addEventListener('change', () => {
   const file = fileInput.files[0];
@@ -7,6 +8,7 @@ fileInput.addEventListener('change', () => {
     const reader = new FileReader();
     reader.onload = () => {
       preview.src = reader.result;
+      imageLink.href = reader.result;
     };
     reader.readAsDataURL(file);
   }
